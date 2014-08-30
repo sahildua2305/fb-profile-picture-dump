25e1ca789c5e3f04fcb81052c88ca5f36c9c8234
Sahil Dua <sahildua2305@gmail.com>
2014-08-31 03:12:15 +0530

    Final working script

--- fb_friends.py
+++ fb_friends.py
@@ -9,28 +9,23 @@ api_url = 'https://graph.facebook.com/v2.1/'
 params = {'access_token' : token}
 
 def extractFriends():
-    call = "me/friends?fields=picture.type(large),gender,name"
+    call = "me/friends?fields=picture.width(9999).height(9999).type(large),gender,name"
     response = requests.get(api_url + call, params=params)
     r = (json.loads(response.content))
     #pprint(r)
     for f in r['data']:
         #print f['name'], f['gender']
         p_url = str(f['picture']['data']['url'])
-        print p_url
-        #webbrowser.open(p_url)
-        #urllib.urlretrieve(p_url, 'something21.jpg')
+        #print p_url
         
         opener1 = urllib2.build_opener()
         page1 = opener1.open(p_url)
         my_picture = page1.read()
 
-        filename = "my_image.jpg"
-        print filename
-        fout = open(filename, "wb")
+        filename = f['name']+"_"+f['id']+".jpg"
+        print filename+" downloaded..."
+        fout = open('images/'+filename, "wb")
         fout.write(my_picture)
         fout.close()
         
-        break
-
-
 extractFriends()
